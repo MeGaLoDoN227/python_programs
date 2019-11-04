639b32abe680500e73506dc704bcf7bb08027239
MeGaLoDoN227 <57372311+MeGaLoDoN227@users.noreply.github.com>
2019-11-04 23:18:53 +0200

    Update and rename Калькулятор.py to Calculator.py

---
+++ Calculator.py
@@ -0,0 +1,24 @@
+a = input('enter the first number:')
+b = input('enter the second number:')
+c = input('action:') 
+r = int(a)
+t = int(b)
+if c == '+':
+    print(r + t)
+elif c == '-':
+    print(r - t)
+if c == '*':
+    print(r * t)
+if c == '/':
+    print(r / t)
+y = input('Your result above')
+
+
+    
+    
+
+    
+    
+
+    
+    
