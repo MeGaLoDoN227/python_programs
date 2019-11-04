0e018a4cfb55e1d9ee9c7c315b1627c721b33af7
MeGaLoDoN227 <57372311+MeGaLoDoN227@users.noreply.github.com>
2019-11-04 23:01:38 +0200

    Add files via upload

---
+++ Калькулятор.py
@@ -0,0 +1,24 @@
+a = input('Введите первое число:')
+b = input('Введите второе число:')
+c = input('Введите действие:') 
+r = int(a)
+t = int(b)
+if c == '+':
+    print(r + t)
+elif c == '-':
+    print(r - t)
+if c == '*':
+    print(r * t)
+if c == '/':
+    print(r / t)
+y = input('Ваш результат сверху')
+
+
+    
+    
+
+    
+    
+
+    
+    
